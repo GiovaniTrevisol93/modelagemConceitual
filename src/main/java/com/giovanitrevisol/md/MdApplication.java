f0c639686cc377de5d337fac5ce04596d015cf82
Giovani Trevisol <giovanitrevisol93@gmail.com>
2019-05-20 01:05:17 -0300

    Projeto criado

---
+++ src/main/java/com/giovanitrevisol/md/MdApplication.java
@@ -0,0 +1,12 @@
+package com.giovanitrevisol.md;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MdApplication.class, args);
+	}
+}
